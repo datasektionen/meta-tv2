ea946b7e881cfbdfd71f80996ffd8a174a992dc2
Daniel Nikolic <dannik@kth.se>
2024-01-30 16:22:57 +0100

    Created layers in backend
    Created data layer (connection to DB), business layer (core functionality), and access layer (API methods)

---
+++ Backend/Meta-TV2-api/Meta-TV2-BusinessLayer/BusinessRules.cs
@@ -0,0 +1,8 @@
+﻿namespace Meta_TV2_BusinessLayer;
+
+public class BusinessRules
+{
+    public void testMethod(){
+        // Do something
+    }
+}
---
+++ Backend/Meta-TV2-api/Meta-TV2-BusinessLayer/IBusinessRules.cs
@@ -0,0 +1,11 @@
+﻿namespace Meta_TV2_BusinessLayer;
+
+public interface IBusinessRules
+{
+    /// <summary>
+    /// Documentations goes in Interface.
+    /// Triple click "/" to auto generate
+    /// documentation template
+    /// </summary>
+    public void testMethod();
+}
