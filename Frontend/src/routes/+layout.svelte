df83be67763b2fd28b0b16706f5d852ac8c2def2
Benjaneb <benjaneb@proton.me>
2024-02-18 18:57:40 +0100

    Simple header and footer

---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,53 @@
+<header>
+    <a href="/"><h1>META-TV</h1></a>
+    <nav>
+        <a href="/history">History</a>
+        <a href="/restore">Restore slides</a>
+        <a href="/login">Log in</a>
+    </nav>
+</header>
+<main>
+    <slot />
+</main>
+<footer>
+    <p>
+        Created with passion by Avid, Benjamin, Daniel N, Daniel S, Dennis,
+        Jacob, Jonathan & Ossian in PVK 2024
+    </p>
+</footer>
+
+<style>
+    header {
+        padding: 0.2em 2em;
+        box-shadow: 0 0 1em #111;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    header a {
+        text-decoration: none;
+        color: black;
+    }
+
+    nav a {
+        margin: 1em;
+        padding: 1em 1.5em;
+        color: white;
+        background-color: #e83c84;
+        border-radius: 0.5em;
+    }
+
+    main {
+        min-height: 75vh;
+    }
+
+    footer {
+        background-color: #eeeeee;
+        padding: 3em 0;
+    }
+
+    footer p {
+        text-align: center;
+    }
+</style>
--- Frontend/src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>META-TV</h1>
-<p>Manage the TV monitors in META.</p>
