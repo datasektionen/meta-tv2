110ab883191268f146944945ef9742e6676a3187
jonathan-blomlof <joning.b@hotmail.com>
2024-02-24 17:34:52 +0100

    Add verify function for local development

--- Backend/Meta-TV2-api/Meta-TV2-BusinessLayer/KthAuth.cs
+++ Backend/Meta-TV2-api/Meta-TV2-BusinessLayer/KthAuth.cs
@@ -1,6 +1,37 @@
-﻿namespace Meta_TV2_BusinessLayer;
+﻿
+using System.Text.Json.Nodes;
+
+namespace Meta_TV2_BusinessLayer;
 
 public class KthAuth : IKthAuth
 {
+    public string VerifyToken(String token)
+    {
+        HttpClient client = new();
 
+        try
+        {
+            client.GetStringAsync("http://localhost:1337/hello");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("\nYou've not set up the test enviroment for login system. That is fine. Defaulting to user: 'turetek'\n");
+            return "turetek";
+        }
+
+        try
+        {
+            // Based on https://github.com/datasektionen/login/
+            // This request should always return if valid, and if not a valid token (or api-key) return status code 4xx
+            Task<String> task = client.GetStringAsync("http://localhost:1337/verify/" + token);
+            String request = task.Result;
+            var details = JsonObject.Parse(request);
+
+            return (string)details["user"];
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
 }
