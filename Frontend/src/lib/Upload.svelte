19255ce9856d4c39b94d92c9537ec4d35e604aca
Benjaneb <benjaneb@proton.me>
2024-02-25 16:56:12 +0100

    Start on upload popup

---
+++ Frontend/src/lib/Upload.svelte
@@ -0,0 +1,25 @@
+<script>
+    let type = 0;
+</script>
+
+<div class="upload-box">
+    <nav>
+        <button>File</button>
+        <button>Link to image</button>
+        <button>Website</button>
+    </nav>
+    <div class="upload-area">
+        {#if type == 0}
+
+        {#elseif type == 1}
+
+        {#elseif type == 2}
+    </div>
+</div>
+
+<style>
+    .upload-box {
+        background-color: white;
+        padding: 1em;
+    }
+</style>
