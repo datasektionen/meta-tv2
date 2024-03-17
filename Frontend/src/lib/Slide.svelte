3d29b5077f5bc9c1213600e52547dbbd2a17babd
occnah <ossian.stange@outlook.com>
2024-03-17 20:38:39 +0100

    Feature: add slide and post component

---
+++ Frontend/src/lib/Slide.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Post from "./Post.svelte";
+
+    export let posts = [1, 2, 3];
+    export let numScreens = 3;
+</script>
+
+<div class="slide-container">
+    {#each posts as post}
+        <Post />
+    {/each}
+</div>
+
+<style>
+    .slide-container {
+        background-color: white;
+        width: 90%;
+        border-radius: 15px;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        padding: 20px;
+        margin: 10px;
+    }
+</style>
