0dc33d799cd2403ab3220b48dbbf670bb2703468
danielsv03 <danielsvensson2003@gmail.com>
2024-03-18 15:41:24 +0100

    Updated documentation

--- Backend/Meta-TV2-api/Meta-TV2-BusinessLayer/IBusinessRules.cs
+++ Backend/Meta-TV2-api/Meta-TV2-BusinessLayer/IBusinessRules.cs
@@ -8,14 +8,39 @@ namespace Meta_TV2_BusinessLayer;
 
 public interface IBusinessRules
 {
+    /// <summary>
+    /// A function that will invoke AddGroup function from the dataLayer
+    /// </summary>
+    /// <param name="groupObject"></param>
+    /// <returns>will return true/false if the operation was sucessfull or not.</returns>
     public Task<bool> AddGroup(string groupObject);
 
+    /// <summary>
+    /// A function that will invoke GetGroups function from the dataLayer, 
+    /// </summary>
+    /// <returns>will return a JSON serialized list of groups if any was found, otherwise null.</returns>
     public Task<string> GetGroups();
 
+    /// <summary>
+    /// Same as getGroups, but will return a single group based on the group id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>JSON serialized group object if found, or null if not found</returns>
     public Task<string> GetGroupById(int id);
 
+    /// <summary>
+    /// Will invoke ArchieveGroup from datalayer, will change archieved attribute to true. 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Will return true if successfull otherwise false</returns>
     public Task<bool> ArchiveGroup(int id);
 
+    /// <summary>
+    /// Will invoke getGroups(page, size) from datalyer
+    /// </summary>
+    /// <param name="page"></param>
+    /// <param name="size"></param>
+    /// <returns>JSON serialized list of groups if found, otherwise null</returns>
     public Task<string> GetGroups(int page, int size);
 
 }
