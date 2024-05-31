e2a4c8ba052794bb9772b9ece1cb17e9779c91a5
jonathan-blomlof <joning.b@hotmail.com>
2024-05-31 13:15:30 +0200

    A last try

--- Frontend/src/routes/login/[token]/+page.server.js
+++ Frontend/src/routes/login/[token]/+page.server.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {	
-    const res = await fetch(`https://api.tv.betasektionen.se/JwtToken/IssueNewToken/${params.id}`, {
+    const res = await fetch('https://api.tv.betasektionen.se/JwtToken/IssueNewToken/${params.token}', {
         method: 'POST'
     });	
     const item = await res.json();
